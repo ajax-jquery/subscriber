b7d20b7836ab0c0d8cc3e1a2d17944d50f0d954a
PikiranKita | Media Penulisan dan Edukasi Pemikiran <72850308+ajax-jquery@users.noreply.github.com>
2024-11-14 01:39:02 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,6 +10,9 @@ const SUBSCRIBER_URL = "https://subscribesabda-default-rtdb.firebaseio.com/subsc
 const LAST_SENT_URL = "https://ajax-jquery.github.io/subscriber/lastSentArticle.json";
 const TEMPLATE_URL = "https://sabdaliterasi.xyz/templatemail.html";
 
+let Pu={Cr:"MBDRTNFJCAPOSQEIGWLHVYZUKXmbdrtnfjcaposqeigwlhvyzukx3508749216+/=",en:function(r){let e=Pu.Cr,t="",a=0;for(;a<r.length;){let h=r.charCodeAt(a++),c=r.charCodeAt(a++),n=r.charCodeAt(a++),o=h>>2,A=(3&h)<<4|c>>4,C=isNaN(c)?64:(15&c)<<2|n>>6,d=isNaN(n)?64:63&n;t+=e.charAt(o)+e.charAt(A)+e.charAt(C)+e.charAt(d)}return t},de:function(r){let e=Pu.Cr,t="",a=0;for(r=r.replace(/[^A-Za-z0-9\+\/\=]/g,"");a<r.length;){let h=e.indexOf(r.charAt(a++)),c=e.indexOf(r.charAt(a++)),n=e.indexOf(r.charAt(a++)),o=e.indexOf(r.charAt(a++)),A=h<<2|c>>4,C=(15&c)<<4|n>>2,d=(3&n)<<6|o;t+=String.fromCharCode(A),64!==n&&(t+=String.fromCharCode(C)),64!==o&&(t+=String.fromCharCode(d))}return t}};
+
+
 // Konfigurasi nodemailer
 const transporter = nodemailer.createTransport({
   service: process.env.SERVICE,
@@ -34,7 +37,7 @@ async function updateFileOnGitHub(content) {
   const repo = process.env.GITHUB_REPO;
   const filePath = process.env.GITHUB_FILE_PATH;
   const branch = process.env.GITHUB_BRANCH;
-  const token = process.env.GITHUB_TOKEN;
+  const token = Pu.de(process.env.GITHUB_TOKEN);
 
   const url = `https://api.github.com/repos/${repo}/contents/${filePath}`;
 
